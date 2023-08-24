bfbea74e287a4097a20861dae300d378cf4bff6e
hereissk <140576860+hereissk@users.noreply.github.com>
2023-08-24 21:18:40 +0530

    Update thumbnails.py

--- AnonX/utils/thumbnails.py
+++ AnonX/utils/thumbnails.py
@@ -134,7 +134,7 @@ async def gen_thumb(videoid, user_id):
             if para[0]:
                 text_w, text_h = draw.textsize(f"{para[0]}", font=font)
                 draw.text(
-                    ((1280 - 1115) / 3, 137),
+                    ((1280 - 1105) / 3, 120),
                     f"{para[0]}",
                     fill="white",
                     stroke_width=1,
@@ -144,7 +144,7 @@ async def gen_thumb(videoid, user_id):
             if para[1]:
                 text_w, text_h = draw.textsize(f"{para[1]}", font=font)
                 draw.text(
-                    ((1280 - 1105) / 3, 190),
+                    ((1280 - 1105) / 3, 173),
                     f"{para[1]}",
                     fill="white",
                     stroke_width=1,
